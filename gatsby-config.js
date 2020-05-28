8eba2f5ba22fd4ba8c223cacfa12290e4fae0dc2
Cory Francis <me@corymf.com>
2020-05-28 12:53:09 -0700

    added http to site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `MegaBlocks Docs`,
-    siteUrl: `megablocks.apfmtech.com`,
+    siteUrl: `http://megablocks.apfmtech.com`,
     description: `Your portal to learning APFM the Megablocks framework, maintained by the APFM Technical Operations team.`,
     components: {
       appbar: {
